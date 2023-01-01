19c982e3872643b41c27e9e7078e7c0470e2f103
Vlad <vladot@ya.ru>
2023-01-01 13:49:22 +0600

    2

--- src/pro/sky/java/course2/hw2_1/Car.java
+++ src/pro/sky/java/course2/hw2_1/Car.java
@@ -2,13 +2,22 @@ package pro.sky.java.course2.hw2_1;
 
 public class Car {
 
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
 
+
+    public Car(String brand, String model, int year, double engineVolume, String color, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
 
     @Override
     public String toString() {
--- src/pro/sky/java/course2/hw2_1/Main.java
+++ src/pro/sky/java/course2/hw2_1/Main.java
@@ -4,57 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car ladaGranta = new Car();
-
-        ladaGranta.brand = "Lada";
-        ladaGranta.model = "Granta";
-        ladaGranta.year = 2015;
-        ladaGranta.country = "Russia";
-        ladaGranta.color = "yellow";
-        ladaGranta.engineVolume = 1.7;
-
-        Car audiA8 = new Car();
-
-        audiA8.brand = "Audi";
-        audiA8.model = "A8";
-        audiA8.year = 2020;
-        audiA8.country = "Germany";
-        audiA8.color = "black";
-        audiA8.engineVolume = 3.0;
-
-        Car bmwZ8 = new Car();
-
-        bmwZ8.brand = "BMW";
-        bmwZ8.model = "Z8";
-        bmwZ8.year = 2021;
-        bmwZ8.country = "Germany";
-        bmwZ8.color = "black";
-        bmwZ8.engineVolume = 3.0;
-
-        Car kiaSportage4 = new Car();
-
-        kiaSportage4.brand = "KIA";
-        kiaSportage4.model = "Sportage 4-th Gen";
-        kiaSportage4.year =2018;
-        kiaSportage4.country = "South Korea";
-        kiaSportage4.color = "red";
-        kiaSportage4.engineVolume = 2.4;
-
-        Car hyundaiAvante = new Car();
-
-        hyundaiAvante.brand = "Hyundai";
-        hyundaiAvante.model = "Avante";
-        hyundaiAvante.year = 2016;
-        hyundaiAvante.country = "South Korea";
-        hyundaiAvante.color = "orange";
-        hyundaiAvante.engineVolume = 1.6;
+        Car ladaGranta = new Car("Lada","Granta", 2015, 1.7, "yellow",
+                "Russia");
+        Car audiA8 = new Car("Audi", "A8", 2020, 3.0, "black",
+                "Germany");
+        Car bmwZ8 = new Car("BMW", "Z8", 2021, 3.0, "black",
+                "Germany");
+        Car kiaSportage4 = new Car("KIA", "Sportage 4-th Gen", 2018, 2.4, "red",
+                "South Korea");
+        Car hyundaiAvante = new Car("Hyundai", "Avante", 2016, 1.6, "orange",
+                "South Korea");
 
         System.out.println(ladaGranta);
         System.out.println(audiA8);
         System.out.println(bmwZ8);
         System.out.println(kiaSportage4);
         System.out.println(hyundaiAvante);
-
 
     }
 }
