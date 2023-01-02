19a639fcedf3153c8d9130801004fb81466275e3
Vlad <vladot@ya.ru>
2023-01-02 21:56:57 +0600

    4,5

--- src/pro/sky/java/course2/hw2_1/Main.java
+++ src/pro/sky/java/course2/hw2_1/Main.java
@@ -1,25 +1,22 @@
 package pro.sky.java.course2.hw2_1;
 
+import pro.sky.java.course2.hw2_1.transport.Car;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Car ladaGranta = new Car("","", 0, 0, "",
-                "");
-        Car audiA8 = new Car("Audi", "A8", 2020, 3.0, "black",
-                "Germany");
-        Car bmwZ8 = new Car("BMW", "Z8", 2021, 3.0, "black",
-                "Germany");
-        Car kiaSportage4 = new Car("KIA", "Sportage 4-th Gen", 2018, 2.4, "red",
-                "South Korea");
-        Car hyundaiAvante = new Car("Hyundai", "Avante", 2016, 1.6, "orange",
-                "South Korea");
+        Car ladaGranta = new Car("","", 0, "", 0,
+                "", "", "", "", 0, false,
+                false, false);
+        Car audiA8 = new Car("Audi", "A8", 3.0, "black", 2000,
+                "Germany", "auto", "sedan", "",5, true,
+                true, true);
+
 
         System.out.println(ladaGranta);
         System.out.println(audiA8);
-        System.out.println(bmwZ8);
-        System.out.println(kiaSportage4);
-        System.out.println(hyundaiAvante);
+
 
     }
 }
